bd3b369bc17393c8d0a5e1c8d445025d1bfd8e0b
IT333 <thanhsang260205@gmail.com>
2025-05-20 11:25:02 +0700

    update url

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog-six-inky-87.vercel.app/",
+	site: "https://blog.s4ngxg.site",
 	base: "/",
 	output: "static",
 	trailingSlash: "always",
